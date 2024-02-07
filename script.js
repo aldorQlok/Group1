22f0825e84802ff2a377f5b32a07685fe26793be
Fridbergs <144354526+Fridbergs@users.noreply.github.com>
2024-02-07 09:43:50 +0100

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// Funktion som loggar "Hej Hej"
-function logHejHej() {
-  console.log("Hej Hej");
-}
-
 // Hitta knappen med ID 'linusKnapp'
 const linusKnapp = document.getElementById("linusKnapp");
 
-// Kontrollera att knappen faktiskt finns
-if (linusKnapp) {
-  // Lägg till en 'click' eventListener
-  linusKnapp.addEventListener("click", logHejHej);
-} else {
-  console.log('Knappen med ID "linusKnapp" hittades inte.');
+linusKnapp.addEventListener("click", logHejHej);
+
+// Funktion som loggar "Hej Hej"
+function logHejHej() {
+  console.log("Hej Hej");
 }
