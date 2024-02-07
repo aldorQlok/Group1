7a010bf3faa232346a455c1eccbab6bd27b4ea4d
Fridbergs <144354526+Fridbergs@users.noreply.github.com>
2024-02-07 10:01:07 +0100

    tredje commit

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Hitta knappen med ID 'linusKnapp'
 const linusKnapp = document.getElementById("linusKnapp");
 
 linusKnapp.addEventListener("click", logHejHej);
 
-// Funktion som loggar "Hej Hej"
 function logHejHej() {
   console.log("Hej Hej");
 }
