d3402c2bf09b5edee28dc7d7f382c56ab3724691
Jumi Strandberg <feliciastrandberg@gmail.com>
2024-02-07 17:37:24 +0900

    jumi done

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const jumiBtn = document.getElementById("jumi");
+    
+    jumiBtn.addEventListener("click", function() {
+        alert("meow");
+    });
+});
